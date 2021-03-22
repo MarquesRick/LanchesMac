53e1755cad905fd9dd71af540adb7dd0d59cb69c
Henrique Marques <dev.sistemas@allis.com.br>
2021-03-21 23:09:17 -0300

    first commit

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,29 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private ILancheRepository _lancheRepository;
+        private ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
---
+++ LanchesMac/Migrations/20210309012922_PopularTables.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Lanches",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Lanches",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemThunbnailUrl",
+                table: "Lanches",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoDetalhada",
+                table: "Lanches",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoCurta",
+                table: "Lanches",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Lanches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Lanches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemThunbnailUrl",
+                table: "Lanches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoDetalhada",
+                table: "Lanches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescricaoCurta",
+                table: "Lanches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
